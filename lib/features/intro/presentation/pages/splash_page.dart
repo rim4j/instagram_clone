488b64fa56070b187c881a6f45a7d519bb6406d1
rim4j <amirhosseinjalali818@gmail.com>
2023-08-04 12:10:41 +0330

    delete animated text in splash screen

--- lib/features/intro/presentation/pages/splash_page.dart
+++ lib/features/intro/presentation/pages/splash_page.dart
@@ -1,8 +1,5 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/common/constants/animations.dart';
-import 'package:instagram_clone/common/constants/strings.dart';
-import 'package:instagram_clone/config/theme/app_styles.dart';
 import 'package:lottie/lottie.dart';
 
 class SplashPage extends StatelessWidget {
@@ -11,15 +8,6 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
-    Size size = MediaQuery.of(context).size;
-    AppFontSize appFontSize = AppFontSize(size: size);
-
-    const colorizeColors = [
-      Colors.purple,
-      Colors.blue,
-      Colors.yellow,
-      Colors.red,
-    ];
 
     return Scaffold(
       backgroundColor: colorScheme.background,
@@ -28,25 +16,6 @@ class SplashPage extends StatelessWidget {
         children: <Widget>[
           Lottie.asset(
             AnimationsConst.instagram,
-          ),
-          SizedBox(
-            width: size.width,
-            child: Center(
-              child: AnimatedTextKit(
-                animatedTexts: [
-                  ColorizeAnimatedText(
-                    Strings.welcome,
-                    textStyle: robotoRegular.copyWith(
-                        fontSize: appFontSize.veryLargeFontSize),
-                    colors: colorizeColors,
-                  ),
-                ],
-                isRepeatingAnimation: false,
-                onTap: () {
-                  print("Tap Event");
-                },
-              ),
-            ),
           ),
         ],
       ),
